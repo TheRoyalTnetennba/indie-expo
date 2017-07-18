bf30ab152bae39162fc0bcf3e76a19fb550752b1
TheRoyalTnetennba <gpaye8@gmail.com>
2017-07-18 15:38:24 -0700

    reorganized controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    namespace :api, defaults: {format: :json} do
+      resource :session
+      resources :users
+      resources :perks
+      resources :contributions
+      resources :categories
+      resources :campaigns
+    end
     root "static_pages#root"
 end
