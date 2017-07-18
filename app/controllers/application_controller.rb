00f2bddabd8c7e7a03c50fbe48ff623d2f01eaae
TheRoyalTnetennba <gpaye8@gmail.com>
2017-07-18 14:44:38 -0700

    add current_user to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def current_user
+    @current_user ||= @user.find_by(session_token: session[:session_token])
+  end
+  
 end
