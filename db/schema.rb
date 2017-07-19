0c7c2c40e1cc6fff4ef23336a11551a34cabe343
TheRoyalTnetennba <gpaye8@gmail.com>
2017-07-18 17:11:17 -0700

    add api_utils

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170718210850) do
+ActiveRecord::Schema.define(version: 20170718234056) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,7 +58,7 @@ ActiveRecord::Schema.define(version: 20170718210850) do
   create_table "users", force: :cascade do |t|
     t.string "username", null: false
     t.string "password_digest", null: false
-    t.string "image_url"
+    t.string "image_url", default: "/assets/defaults/user_profile_thumbnail.png", null: false
     t.text "bio"
     t.string "email", null: false
     t.string "session_token"
