063f6151ad38f23a4309787fcfbfcfa002bf785c
TheRoyalTnetennba <gpaye8@gmail.com>
2017-07-20 21:28:06 -0700

    fixed type in campaign model

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,7 +1,7 @@
 class Campaign < ApplicationRecord
   validates :title, :goal, :creator_id, :category_id, :archived
 
-  belongs_to :creator
+  belongs_to :creator,
     primary_key: :id,
     foreign_key: :creator_id,
     class_name: :User
