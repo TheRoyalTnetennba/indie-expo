fe0fdf0e17addce4af9b7de6e13c680d58de42c1
TheRoyalTnetennba <gpaye8@gmail.com>
2017-09-04 16:06:56 -0700

    add google analytics

--- index.jsx
+++ index.jsx
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom';
 import Root from './frontend/root';
 import configureStore from './frontend/store/store';
 import Modal from 'react-modal';
+import ReactGA from 'react-ga';
+
+ReactGA.initialize('UA-103955267-3');
+
+function logPageView() {
+  ReactGA.set({ page: window.location.pathname + window.location.search });
+  ReactGA.pageview(window.location.pathname + window.location.search);
+}
 
 document.addEventListener('DOMContentLoaded', () => {
   let store;
@@ -16,5 +24,5 @@ document.addEventListener('DOMContentLoaded', () => {
   window.store = store;
   Modal.setAppElement(document.body);
   const root = document.getElementById('root');
-  ReactDOM.render(<Root store={store} />, root);
+  ReactDOM.render(<Root store={store} onUpdate={logPageView} />, root);
 });
